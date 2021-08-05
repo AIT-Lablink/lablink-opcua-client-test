4c2e53606bc5808864d0b3a33feba212349765b3
Edmund Widl <edmund.e.widl@ieee.org>
2021-08-05 16:24:51 +0200

    Fix example scripts: allow path names with whitespace

--- setup/3_run_test/csv-client.cmd
+++ setup/3_run_test/csv-client.cmd
@@ -3,7 +3,7 @@
 SETLOCAL
 
 REM Load the setup for the examples.
-CALL %~DP0\..\setup.cmd
+CALL "%~DP0\..\setup.cmd"
 
 REM CSV client configuration.
 SET CONFIG_FILE_URI=%LLCONFIG%ait.test.opcua.csvclient.config
@@ -12,6 +12,6 @@ REM Logger configuration.
 SET LOGGER_CONFIG=-Dlog4j.configurationFile=%LLCONFIG%ait.all.log4j2
 
 REM Run the example.
-"%JAVA_HOME%\bin\java.exe" %LOGGER_CONFIG% -jar %CSV_CLIENT_JAR_FILE% -c %CONFIG_FILE_URI%
+"%JAVA_HOME%\bin\java.exe" %LOGGER_CONFIG% -jar "%CSV_CLIENT_JAR_FILE%" -c %CONFIG_FILE_URI%
 
 PAUSE
