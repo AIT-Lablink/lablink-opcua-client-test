c4c2419fbcc570ee64dc2bb83516125d6d39e865
Edmund Widl <edmund.e.widl@ieee.org>
2022-02-02 12:17:20 +0100

    update Lablink dependencies

--- setup/setup.cmd
+++ setup/setup.cmd
@@ -11,22 +11,22 @@ REM Root directory of this Lablink setup (only change this if you really know wh
 SET SETUP_ROOT_DIR=%~DP0..
 
 REM Path to Java JAR file of the OPC UA client package.
-SET OPCUA_CLIENT_JAR_FILE=%SETUP_ROOT_DIR%\target\dependency\opcuaclient-0.0.2-jar-with-dependencies.jar
+SET OPCUA_CLIENT_JAR_FILE=%SETUP_ROOT_DIR%\target\dependency\opcuaclient-0.0.3-jar-with-dependencies.jar
 
 REM Path to Java JAR file of CSV client.
-SET CSV_CLIENT_JAR_FILE=%SETUP_ROOT_DIR%\target\dependency\csvclient-0.0.1-jar-with-dependencies.jar
+SET CSV_CLIENT_JAR_FILE=%SETUP_ROOT_DIR%\target\dependency\csvclient-0.0.2-jar-with-dependencies.jar
 
 REM Path to Java JAR file of data plotter.
-SET PLOT_JAR_FILE=%SETUP_ROOT_DIR%\target\dependency\plotter-0.0.2-jar-with-dependencies.jar
+SET PLOT_JAR_FILE=%SETUP_ROOT_DIR%\target\dependency\plotter-0.0.4-jar-with-dependencies.jar
 
 REM Path to Java JAR file of data point bridge.
-SET DPB_JAR_FILE=%SETUP_ROOT_DIR%\target\dependency\dpbridge-0.0.1-jar-with-dependencies.jar
+SET DPB_JAR_FILE=%SETUP_ROOT_DIR%\target\dependency\dpbridge-0.0.2-jar-with-dependencies.jar
 
 REM Path to Java JAR file of config server.
-SET CONFIG_JAR_FILE=%SETUP_ROOT_DIR%\target\dependency\config-0.1.0-jar-with-dependencies.jar
+SET CONFIG_JAR_FILE=%SETUP_ROOT_DIR%\target\dependency\config-0.1.1-jar-with-dependencies.jar
 
 REM Check if environment variable JAVA_HOME has been defined.
 IF NOT DEFINED JAVA_HOME (
-    ECHO WARNING: environment variable JAVA_HOME not has been defined!
+    ECHO WARNING: environment variable JAVA_HOME has not been defined!
     PAUSE
 )
